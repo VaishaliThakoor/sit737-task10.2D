395ce5f7e933027b993ffbf71039036e932c9ecc
mthakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2023-06-08 21:38:19 +1000

    new changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('I made some changes');
+  res.send('some changes have been made');
 });
 
 app.listen(PORT, HOST, () => {
